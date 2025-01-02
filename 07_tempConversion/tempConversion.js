f33852d9742feed661f9a765036bb0737837df7e
chadelz-dev <133863404+chadelz-dev@users.noreply.github.com>
2025-01-02 18:26:43 +0200

    Ex7 Completed - javascript-exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let finalTemp = Math.round(((fTemp - 32) * (5/9)) * 10) / 10;
+  return finalTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let finalTemp = Math.round((cTemp * 9 / 5 + 32) * 10) / 10;
+  return finalTemp;
 };
 
 // Do not edit below this line
